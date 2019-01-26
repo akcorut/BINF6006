7b4b956814a32078d6237896bda5663afed9eab0
Kivanc Corut <kivanc.corut@uga.edu>
2019-01-26 17:07:27 -0500

    convert double to string

--- java_training/floatingDigits.java
+++ java_training/floatingDigits.java
@@ -13,11 +13,11 @@ public class floatingDigits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Enter a float number: ");
+        System.out.print("Enter a floating-point number: ");
         double number = input.nextDouble();
 
-        String str_temp = new Double(number).toString();
-        String str = str_temp.substring(0,str_temp.indexOf('.'));
+        String str_temp = String.format("%f", number); // Convert double to String
+        String str = str_temp.substring(0,str_temp.indexOf('.')); // Get substring before point
         int digits = str.length();
         
         System.out.print(digits);
