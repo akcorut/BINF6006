4fc94775338b2ec83c49d7e69f964e6f39536c8c
Kivanc Corut <kivanc.corut@uga.edu>
2019-01-31 14:19:46 -0500

    max method

--- java_training/methods.java
+++ java_training/methods.java
@@ -5,9 +5,19 @@ public class methods {
             sum += i;
         return sum;
     }
+    public static int max(int i, int j) {
+        if (i > j) {
+            return i;
+        } else {
+            return j;
+        }
+    }
     public static void main(String[] args) {
+        
         System.out.println("Sum from 1 to 10 is " + sum(1, 10));
         System.out.println("Sum from 20 to 37 is " + sum(20, 37));
         System.out.println("Sum from 35 to 49 is " + sum(35, 49));
+
+        System.out.println("The bigger number is " + max(15, 234));
     }
 }
