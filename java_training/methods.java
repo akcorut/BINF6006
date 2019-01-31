ddcf011ebbef1b8eb4bb7d79c6329776cfe483c6
Kivanc Corut <kivanc.corut@uga.edu>
2019-01-31 14:24:41 -0500

    minor changes

--- java_training/methods.java
+++ java_training/methods.java
@@ -13,11 +13,9 @@ public class methods {
         }
     }
     public static void main(String[] args) {
-        
         System.out.println("Sum from 1 to 10 is " + sum(1, 10));
         System.out.println("Sum from 20 to 37 is " + sum(20, 37));
         System.out.println("Sum from 35 to 49 is " + sum(35, 49));
-
         System.out.println("The bigger number is " + max(15, 234));
     }
 }
