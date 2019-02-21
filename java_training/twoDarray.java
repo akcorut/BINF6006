02c5b726e2095a34feb07763a3e092a5e20d5a16
Kivanc Corut <kivanc.corut@uga.edu>
2019-02-21 14:05:17 -0500

    Homework3

--- java_training/twoDarray.java
+++ java_training/twoDarray.java
@@ -1,3 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 public class twoDarray {
-    
+    public static void main(String[] args) throws FileNotFoundException{
+        File text = new File("/Users/kivanccorut/Desktop/courses_uga/spring_2019/BINF_6006/BINF6006/java_training/test.txt");
+        Scanner scnr = new Scanner(text);
+        PrintWriter grades = new PrintWriter("/Users/kivanccorut/Desktop/courses_uga/spring_2019/BINF_6006/BINF6006/java_training/result.txt");
+
+        while (scnr.hasNextLine()) {
+            double n1 = scnr.nextDouble();
+            grades.println(n1);
+        }
+        grades.close();
+        scnr.close();
+    }
 }
