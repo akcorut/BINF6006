b1da3154146bbea86e958b99f09720ed37e9ec95
Kivanc Corut <kivanc.corut@uga.edu>
2019-02-21 16:33:26 -0500

    Add homework 3

--- java_training/twoDarray.java
+++ java_training/twoDarray.java
@@ -1,3 +1,7 @@
+/** 
+ * @author Adnan Kivanc Corut
+ */
+
 import java.util.*;
 import java.io.*;
 
@@ -8,8 +12,16 @@ public class twoDarray {
         PrintWriter grades = new PrintWriter("/Users/kivanccorut/Desktop/courses_uga/spring_2019/BINF_6006/BINF6006/java_training/result.txt");
 
         while (scnr.hasNextLine()) {
-            double n1 = scnr.nextDouble();
-            grades.println(n1);
+            Double sum = 0.0;
+            Double avg = 0.0;
+            Double g1 = scnr.nextDouble();
+            sum += g1;
+            Double g2 = scnr.nextDouble();
+            sum += g2;
+            Double g3 = scnr.nextDouble();
+            sum += g3;
+            avg = (sum/3);
+            grades.println(g1 + " " + g2 + " " + g3 + " " + avg);
         }
         grades.close();
         scnr.close();
