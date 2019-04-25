cdd3407aa6c87fb73f28e34d5966a84182d1ca1e
Kivanc Corut <kivanc.corut@uga.edu>
2019-04-25 14:53:46 -0400

    Add dlTest.java

---
+++ java_training/doubleyLinkedList/dlTest.java
@@ -0,0 +1,13 @@
+public class dlTest {
+    public static void main(String[] args) {
+        DList dList = new DList();
+
+        dList.addNode(1);
+        dList.addNode(2);
+        dList.addNode(3);
+        dList.addNode(4);
+        dList.addNode(5);
+
+        dList.display();
+    }
+}
